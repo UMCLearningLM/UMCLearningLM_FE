9944bfbd4eae5b07e73498aceb837f437af95e53
picnic88 <ssemilife@gmail.com>
2026-07-18 04:43:55 +0900

    각 요소별 간격 노출  수정 대기

--- LearningLM/src/App.tsx
+++ LearningLM/src/App.tsx
@@ -1,7 +1,8 @@
 import { HomePage } from './pages/home/HomePage'
+import { Stdio_create1 } from './pages/Stdio_create1'
 
 function App() {
-  return <HomePage />
+  return <Stdio_create1 />
 }
 
 export default App
---
+++ LearningLM/src/pages/Stdio_create1.tsx
@@ -0,0 +1,49 @@
+import { Header } from "../components/layout/Header";
+
+export function Stdio_create1() {
+
+    return (
+        <>
+            <div className="w-[1920px] min-h-[2528px] bg-[#F5F5F7]">
+                <Header />
+                {/*블록 팔레트 */}
+                <div className="relative z-30 w-[358px] bg-white flex flex-col items-center justify-center">
+                    {/*블록 팔레트 텍스트 section */}
+                    <div className="h-[136px] flex flex-col items-center justify-center">
+
+                    </div>
+                    {/*블록 팔레트 텍스트 하단 section */}
+                    <div>
+                        {/*블록 검색 */}
+                        <div>
+
+                        </div>
+                        {/*입력 section */}
+                        <div>
+                        </div>
+                        {/*컨텍스트 section */}
+                        <div>
+                        </div>
+                        {/*프로세스 section */}
+                        <div>
+                        </div>
+                        {/*검토 section */}
+                        <div>
+                        </div>
+                        {/*결과 section */}
+                        <div>
+                        </div>
+                    </div>
+                </div>
+                {/*인스펙터 */}
+                <div className="relative z-30">
+
+                </div>
+                {/*메인 화면 */}
+                <div className="relative z-10">
+
+                </div>
+            </div>
+        </>
+    )
+}
