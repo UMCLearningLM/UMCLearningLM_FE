75d63b9a9993ca6b2aa865593e1616554e090e66
picnic88 <ssemilife@gmail.com>
2026-07-25 01:30:00 +0900

    블록튜토리얼-Studio_create1 페이지 화면구현

--- LearningLM/src/main.tsx
+++ LearningLM/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import "@xyflow/react/dist/style.css";
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
