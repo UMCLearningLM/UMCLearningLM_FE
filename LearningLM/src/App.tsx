8593db54e9bbc28708fe502bc28beebe15f15120
picnic88 <ssemilife@gmail.com>
2026-07-25 16:58:18 +0900

    npm install lucide-react 라이브러리 설치

--- LearningLM/src/App.tsx
+++ LearningLM/src/App.tsx
@@ -1,7 +1,8 @@
 import { HomePage } from './pages/home/HomePage'
+import { Studio_create_review1 } from './pages/Studio_create_review_details1'
 
 function App() {
-  return <HomePage />
+  return <Studio_create_review1 />
 }
 
 export default App
