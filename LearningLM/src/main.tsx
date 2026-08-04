2b3178a7fbb255cee6fd497fd7fbdae20e20cdcf
picnic88 <ssemilife@gmail.com>
2026-08-04 18:57:40 +0900

    임시로 수정했던 main 코드 원상복구

--- LearningLM/src/main.tsx
+++ LearningLM/src/main.tsx
@@ -5,19 +5,12 @@ import '@xyflow/react/dist/style.css'
 import './index.css'
 
 import App from './App'
-import { RV_001 } from './review/RV_001'
-import { RV_002 } from './review/RV_002'
-import { RV_003 } from './review/RV_003'
-import { RV_004 } from './review/RV_004'
-import { RV_005 } from './review/RV_005'
-import { RV_006 } from './review/RV_006'
-import { RV_007 } from './review/RV_007'
-import { RV_010 } from './review/RV_010'
+
 
 createRoot(
   document.getElementById('root')!,
 ).render(
   <StrictMode>
-    <RV_007 />
+    <App />
   </StrictMode>,
 )
